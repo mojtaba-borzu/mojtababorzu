28d3228b263b6b9a111c34ab39619ea32d7834f6
mojtaba borzu <mojtaba.borzu69@gmail.com>
2023-10-04 11:51:54 +0330

    change className

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <main className={`flex min-h-screen flex-col items-center  p-24 `}>
         <div className="flex flex-col items-center ">
           <h2 className="sm:text-[80px] text-[30px]">Mojtaba Borzu</h2>
-          <h1 className="sm:text-[50px] text-[20px]">مجتبی برزو</h1>
+          <h1 className="sm:text-[50px] text-[20px] ">مجتبی برزو</h1>
         </div>
         <div className="flex flex-col items-center ">
           <h3 className="sm:text-[80px] text-[30px]">Front End </h3>
